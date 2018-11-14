f0f92f42a4b3e90dda096b3b217e78bcb2468ead
Master <app@yydrobot.com>
2018-11-14 09:19:16 +0800

    finish ex9

--- src/L3ex7.kt
+++ src/L3ex7.kt
@@ -17,7 +17,22 @@ fun getBirthday() : Int {
     return birthday
 }
 
-fun getFortune
+fun getFortune(birthday : Int) : String {
+    val fortune = listOf<String>("You will have a great day!"
+            , "Things will go well for you today."
+            , "Enjoy a wonderful day of success."
+            , "Be humble and all will turn out well."
+            , "Today is a good day for exercising restraint."
+            , "Take it easy and enjoy life!"
+            , "Treasure your friends because they are your greatest fortune.")
+
+    val index = when (birthday) {
+        in 1..7 -> 4
+        28, 31 -> 2
+        else -> birthday.rem(fortune.size)
+    }
+    return fortune[index]
+}
 
 fun getFortuneCookie() : String {
     val fortuneCookie = listOf<String>("You will have a great day!"
