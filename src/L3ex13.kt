944bfbeb9dd44bb3de3fd9fb46847f77378bc04f
Master <app@yydrobot.com>
2018-11-16 17:53:42 +0800

    Initial ex13 file

---
+++ src/L3ex13.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println(whatShouldIDoToday("happy"))
+}
+
+fun whatShouldIDoToday(mood: String, weather: String = "Sunny", temperature: Int = 24) : String {
+    return when {
+        mood == "happy" && weather == "Sunny" -> "go for a walk."
+        else -> "Stay home and read."
+    }
+}
